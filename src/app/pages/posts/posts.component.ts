0d3b609028c6a65296481ceab1c812caa0d0be0e
ArtN0nymous <jr616934@gmail.com>
2023-03-13 09:41:11 -0700

    mostrar datos en html

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
-
+export class PostsComponent implements OnInit{
+  messages:any;
+  constructor( private dataService:DataService){}
+  ngOnInit(): void {
+      this.messages= this.dataService.getPost();
+  }
 }
